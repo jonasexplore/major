3a411483c6402332251c7673d11e8b8f4145bc51
Jonas <jonasexplore@gmail.com>
2022-04-09 22:11:02 -0300

    refactor: update classroom service

---
+++ classroom/src/http/http.module.ts
@@ -0,0 +1,19 @@
+import path from 'node:path';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+
+import { DatabaseModule } from 'src/database/database.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+})
+export class HttpModule {}
