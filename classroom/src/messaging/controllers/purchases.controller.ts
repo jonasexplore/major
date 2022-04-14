19c3d59d9d2864175f25d1d336512c7b6fc04279
Jonas <jonasexplore@gmail.com>
2022-04-14 09:55:51 -0300

    feat(messaging): finished integration with messaging kafka

---
+++ classroom/src/messaging/controllers/purchases.controller.ts
@@ -0,0 +1,56 @@
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import { CoursesService } from '../../services/courses.service';
+import { EnrollmentsService } from '../../services/enrollments.service';
+import { StudentsService } from '../../services/students.service';
+
+export interface Customer {
+  authUserId: string;
+}
+
+export interface Product {
+  id: string;
+  title: string;
+  slug: string;
+}
+
+export interface PurchaseCreatedPayload {
+  customer: Customer;
+  product: Product;
+}
+
+@Controller()
+export class PurchaseController {
+  constructor(
+    private studentsService: StudentsService,
+    private coursesService: CoursesService,
+    private enrollmentsService: EnrollmentsService,
+  ) {}
+
+  @EventPattern('purchases.created')
+  async purchaseCreated(@Payload('value') payload: PurchaseCreatedPayload) {
+    let student = await this.studentsService.findByAuthUserId(
+      payload.customer.authUserId,
+    );
+
+    if (!student) {
+      student = await this.studentsService.create({
+        authUserId: payload.customer.authUserId,
+      });
+    }
+
+    let course = await this.coursesService.findBySlug(payload.product.slug);
+
+    if (!course) {
+      course = await this.coursesService.createCourse({
+        title: payload.product.title,
+        slug: payload.product.slug,
+      });
+    }
+
+    await this.enrollmentsService.create({
+      courseId: course.id,
+      studentId: student.id,
+    });
+  }
+}
