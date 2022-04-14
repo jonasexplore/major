7249fc12ae865484e960b576bc901cfb22a06dd2
Jonas <jonasexplore@gmail.com>
2022-04-14 09:54:45 -0300

    feat(types): added new types

--- classroom/src/types/create-course.ts
+++ classroom/src/types/create-course.ts
@@ -1,3 +1,4 @@
 export type CreateCourse = {
   title: string;
+  slug?: string;
 };
--- classroom/src/types/index.ts
+++ classroom/src/types/index.ts
@@ -1,3 +1,5 @@
 export { CreateCourse } from './create-course';
 export { FindByCourseAndStudentId } from './find-by-course-and-student';
 export { AuthUser } from './auth-user';
+export { CreateStudent } from './create-student';
+export { CreateEnrollment } from './create-enrollment';
